05340c8fb02ca8b9a32fa2e2663769559a6d5c44
DesWolf <admin@deswolf.com>
2014-02-17 23:27:30 +0000

    Create jquery.newtemat.js

---
+++ jquery.newtemat.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+	$('.tborder input[name=subject]').focusout(function() {
+		// Wywal wiecej niz 1 ! ?, usun spacje przed znakami:
+		var regex = /\s+([?.,!"':])/g;
+		var titleText = $('input[name=subject]').val().replace(regex, '$1').replace(/\?+/g,'?').replace(/\!+/g,'!'); 
+		// Przepisz tytul:
+		$('input[name=subject]').val(titleText);
+	});
+});
