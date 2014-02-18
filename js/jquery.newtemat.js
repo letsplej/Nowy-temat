b615ff4fdee513930f8880f199d14d78532f6f9e
DesWolf <admin@deswolf.com>
2014-02-18 21:14:33 +0000

    Update jquery.newtemat.js

--- js/jquery.newtemat.js
+++ js/jquery.newtemat.js
@@ -1,4 +1,11 @@
 jQuery(document).ready(function($) {
+	$('.tborder input[name=subject]').keyup(function(event) {
+		var textBox = event.target;
+		var start = textBox.selectionStart;
+		var end = textBox.selectionEnd;
+		textBox.value = textBox.value.charAt(0).toUpperCase() + textBox.value.slice(1);
+		textBox.setSelectionRange(start, end);
+	});
 	$('.tborder input[name=subject]').focusout(function() {
 		// Wywal wiecej niz 1 ! ?, usun spacje przed znakami:
 		var regex = /\s+([?.,!:])/g;
